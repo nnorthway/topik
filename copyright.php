17e496505e41054597bd5b1d36ef2bb5ddce09df
Nate Northway <northwayn@gmail.com>
2021-01-22 11:08:11 -0600

    Create copyright file

---
+++ copyright.php
@@ -0,0 +1,27 @@
+<?php
+$title = "Copyright/Licensing";
+include 'header.php';
+?>
+<div class='section'>
+  <div class='container'>
+    <div class='columns is-centered'>
+      <div class='column is-four-fifths'>
+        <h1 class='is-size-1'>Copyright and Licensing</h1>
+        <p>
+          This program is free software: you can redistribute it and/or modify
+          it under the terms of the GNU General Public License as published by
+          the Free Software Foundation, either version 3 of the License, or
+          (at your option) any later version.
+          <br /><br />
+          This program is distributed in the hope that it will be useful,
+          but WITHOUT ANY WARRANTY; without even the implied warranty of
+          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+          GNU General Public License for more details.
+          <br /><br />
+          You should have received a copy of the GNU General Public License
+          along with this program. If not, see <a href='https://www.gnu.org/licenses/'>https://www.gnu.org/licenses/</a>.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
